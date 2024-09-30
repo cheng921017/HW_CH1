d7b150847623cfc30cbbabd10099032b24ff5c7d
cheng921017 <t111360139@ntut.org.tw>
2024-09-30 15:33:51 +0800

    Add files via upload

---
+++ Sample8.java
@@ -0,0 +1,10 @@
+public class Sample8 {
+    public static void main(String[] args) {
+        int num;
+        num = 3;
+        System.out.println("變數num的值是" + num);
+        num = 5;
+        System.out.println("更新變數num的值");
+        System.out.println("更新變數num後的值是" + num);
+    }
+}
