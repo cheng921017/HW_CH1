a91ec99430e6716df8ab88105e8108be10231681
cheng921017 <t111360139@ntut.org.tw>
2024-10-03 21:08:57 +0800

    Update Sample5.java

--- Sample5.java
+++ Sample5.java
@@ -1,5 +1,7 @@
-public class Sample5 {
-    public static void main(String[] args) {
+public class Sample5 
+{
+    public static void main(String[] args) 
+    {
         System.out.println("八進位數101的字元是\101");
         System.out.println("十六進位數0061的字元是\u0061");
     }
