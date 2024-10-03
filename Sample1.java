426856313b5b88bf65a872d80e31c86ef9187a00
cheng921017 <t111360139@ntut.org.tw>
2024-10-03 21:07:33 +0800

    Update Sample1.java

--- Sample1.java
+++ Sample1.java
@@ -1,5 +1,6 @@
 public class Sample1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         System.out.print("歡迎使用Java!");
         System.out.print("開始使用Java吧!");
     }
