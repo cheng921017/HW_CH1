a2566fc237137a93066458daa913fd59e3f722d0
cheng921017 <t111360139@ntut.org.tw>
2024-10-03 21:06:52 +0800

    Update Sample7.java

--- Sample7.java
+++ Sample7.java
@@ -1,5 +1,6 @@
 public class Sample7 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         int num;
         num = 3;
         System.out.println("變數num的值是" + num);
