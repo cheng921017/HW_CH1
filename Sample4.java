6588ecb2f040ee8eab6ce8079ac38b9488f190de
cheng921017 <t111360139@ntut.org.tw>
2024-10-03 21:04:59 +0800

    Update Sample4.java

--- Sample4.java
+++ Sample4.java
@@ -1,6 +1,6 @@
 public class Sample4 {
     public static void main(String[] args) {
-        System.out.println("歡迎使用Java!");
-        System.out.println("歡迎使用Java!");
+        System.out.println("顯示出反斜線:\\");
+        System.out.println("顯示出單引號\'");
     }
 }
