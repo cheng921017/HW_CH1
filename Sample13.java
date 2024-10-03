f4dedecff68749a49356a8dd92b1af9ad424de0e
cheng921017 <t111360139@ntut.org.tw>
2024-10-03 21:18:07 +0800

    Update Sample13.java

--- Sample13.java
+++ Sample13.java
@@ -1,5 +1,7 @@
-public class Sample13 {
-    public static void main(String[] args) {
+public class Sample13
+    {
+    public static void main(String[] args) 
+        {
         double dnum = 160.5;
         System.out.println("身高是" + dnum + "公分");
         System.out.println("指定給int型態的變數");
