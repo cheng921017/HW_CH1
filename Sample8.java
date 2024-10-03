dec20c7ba372dc816b50c374f058e510a85d2c49
cheng921017 <t111360139@ntut.org.tw>
2024-10-03 21:12:54 +0800

    Update Sample8.java

--- Sample8.java
+++ Sample8.java
@@ -3,9 +3,9 @@ public class Sample8
     public static void main(String[] args) 
     {
         int num=3;
-        System.out.println("變數num的值是"+num);
+        System.out.println("變數num的值是" + num);
         num = 5;
         System.out.println("更新變數num的值");
-        System.out.println("更新變數num後的值是"+num);
+        System.out.println("更新變數num後的值是" + num);
     }
 }
